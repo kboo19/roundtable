d5db619f839527b53322cb0ca7e0f61bc1f58889
Khalil Bouaazzi <kbouaazzi@gmail.com>
2020-02-15 13:48:19 -0600

    removed branding from navbar

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,7 +1,7 @@
 <nav class="navbar" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
     <a class="navbar-item" href="http://ordermygear.com">
-      <img src="images/omg.webp" alt="logo">
+      OMG
     </a>
   </div>
 
